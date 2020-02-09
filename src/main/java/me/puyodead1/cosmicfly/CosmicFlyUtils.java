4735a79195e3022324cda203fcd67841c6384e21
Puyodead1 <23562356riley@gmail.com>
2020-02-09 01:07:04 -0500

    Update License Headers

--- src/main/java/me/puyodead1/cosmicfly/CosmicFlyUtils.java
+++ src/main/java/me/puyodead1/cosmicfly/CosmicFlyUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020. Puyodead1
+ * Copyright (c) 2020 Puyodead1
  * This work is licensed under a Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License. (https://creativecommons.org/licenses/by-nc-nd/4.0/)
  */
 
